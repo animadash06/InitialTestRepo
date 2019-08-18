4bcb44de26afc18e2ebc8a92fae489a91714cf62
animadash06 <54240622+animadash06@users.noreply.github.com>
2019-08-18 19:51:16 +0530

    First comit

---
+++ app.py
@@ -0,0 +1,22 @@
+def readStudentdDetails():
+    print("Enter the number of students: ")
+    numberofStudents = int(input())
+
+    pass
+def rankStudents():
+    pass
+def rewardStudents():
+    pass
+def appreciateStudents():
+    pass
+readStudentsDetails()
+
+
+
+
+
+
+
+
+
+
